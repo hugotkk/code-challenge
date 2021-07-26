c88fdc99ebd5562e696f43e9fc514a0d8c729c8c
hugo tse <hugotse@theorigo.com>
2021-07-26 22:10:24 +0100

    Add controller for testing

--- survey/application/config/routes.php
+++ survey/application/config/routes.php
@@ -50,5 +50,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'main';
+if(ENVIRONMENT == 'development') {
+    $route['test'] = 'test';
+}
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
